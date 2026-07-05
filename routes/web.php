836b79f88969ac6a57ba4bfc91cc75a8057d5d90
Yelma Rapiani <yelmarapiani@gmail.com>
2026-07-05 16:16:58 +0800

    Pertemuan 11: layouting

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\DasboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ DasboardController::class, 'index'])->name('dasboard.index');
+Route::resource('/user', UserController::class);
